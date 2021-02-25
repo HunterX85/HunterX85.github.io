e57a6a33f7587d21484866d324840de022357552
Sergey <68477622+HunterX85@users.noreply.github.com>
2021-02-25 17:24:40 +0200

    Portfolio files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+var $page = $('html, body');
+$('a[href*="#"]' ).click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+    return false;
+}); */
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-250px";
+  }
+  prevScrollpos = currentScrollPos;
+}

667ffef0c6a67f8369b31021c15d87493d08381f
Sergey <68477622+HunterX85@users.noreply.github.com>
2021-02-28 22:24:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-/*
+
 var $page = $('html, body');
 $('a[href*="#"]' ).click(function() {
     $page.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
     return false;
-}); */
+}); 
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
